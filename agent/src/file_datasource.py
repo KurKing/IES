90ed167b72187984d1e29d368058436acfc4d31e
KurKing <lyosha.kurkin@gmail.com>
2024-04-17 02:56:11 +0300

    - complete lab 1

--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -1,11 +1,9 @@
-from csv import reader
 from datetime import datetime
 from domain.accelerometer import Accelerometer
 from domain.parking import Parking
 from domain.gps import Gps
 from domain.aggregated_data import AggregatedData
-import config
-
+import csv
 
 class FileDatasource:
     def __init__(
@@ -14,19 +12,66 @@ class FileDatasource:
         gps_filename: str,
         parking_filename: str,
     ) -> None:
-        pass
+        
+        self.is_reading_stoped = False
+
+        self.accelerometer_filename = accelerometer_filename
+        self.accelerometer_data = []
+        self.accelerometer_data_reading_finished = False
+
+        self.gps_filename = gps_filename
+        self.gps_data = []
+        self.gps_reading_reading_finished = False
+
+        self.parking_filename = parking_filename
+        self.parking_data = []
+        self.parking_reading_reading_finished = False
 
     def read(self) -> AggregatedData:
+        if self.is_reading_stoped: return;
+    
+        acc_data = [0] * 3
+        if len(self.accelerometer_data) > 0:
+            acc_data = list(map(float, self.accelerometer_data.pop(0)))
+        else :
+            self.accelerometer_data_reading_finished = True
+
+        gps_data = [0] * 2
+        if len(self.gps_data) > 0:
+            gps_data = list(map(float, self.gps_data.pop(0)))
+        else :
+            self.gps_reading_reading_finished = True
+
+        park_data = [0] * 3
+        if len(self.parking_data) > 0:
+            park_data = list(map(float, self.parking_data.pop(0)))
+        else :
+            self.parking_reading_reading_finished = True
+
+        self.stopReading()
+
         """Метод повертає дані отримані з датчиків"""
         return AggregatedData(
-            Accelerometer(1, 2, 3),
-            Gps(4, 5),
+            Accelerometer(*acc_data),
+            Gps(*gps_data),
             datetime.now(),
-            Parking(3, Gps(5,4))
+            Parking(park_data[0], Gps(park_data[1],park_data[2]))
         )
 
     def startReading(self, *args, **kwargs):
-        """Метод повинен викликатись перед початком читання даних"""
+        
+        def read_csv(filename):
+            data = []
+            with open(filename, 'r') as file:
+                csv_reader = csv.reader(file)
+                for row in csv_reader:
+                    data.append(row)
+            return data[1:]
+        
+        self.accelerometer_data = read_csv(self.accelerometer_filename)
+        self.gps_data = read_csv(self.gps_filename)
+        self.parking_data = read_csv(self.parking_filename)
 
     def stopReading(self, *args, **kwargs):
         """Метод повинен викликатись для закінчення читання даних"""
+        self.is_reading_stoped = self.accelerometer_data_reading_finished and self.gps_reading_reading_finished and self.parking_reading_reading_finished
